a6272d6e034a569100684af9b071eaf70f46a861
Eduardo Spillere Anzolin <eduardospillereanzolin@gmail.com>
2016-10-31 15:06:30 -0200

    Programa finalizado

--- TrabalhoHash/func_arquivo.h
+++ TrabalhoHash/func_arquivo.h
@@ -1,91 +1,96 @@
 /*
- *
- *   classe para criar o txt, ler
- *   ou seja realiza manuipulação e manda para o gerenciador de data
- *
- */
+*
+*   classe para criar o txt, ler
+*   ou seja realiza manuipulação e manda para o gerenciador de data
+*
+*/
 
-#
-include < iostream > #include < fstream > #include < string.h > #include < cstdlib > #include < map > #include < sstream >
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include <cstdlib>
+#include <map>
+#include <sstream>
 
 
- using namespace std;
+using namespace std;
 
 pessoa p;
 hash h;
 
-class func_arquivo {
- public:
-  func_arquivo();
- void cria_arquivo();
- void limpar_arquivo();
- void inicia_data();
+class func_arquivo{
+    public:
+        func_arquivo();
+        void cria_arquivo();
+        void limpar_arquivo();
+        void inicia_data();
 };
 
 
 //construtor
-func_arquivo::func_arquivo() {
+func_arquivo::func_arquivo(){
 
 
 }
 
 //função capaz de criar o arquivo e escrever dentro
-void func_arquivo::cria_arquivo() {
- ofstream arquivo;
- list < pessoa > ::iterator it;
- for (int i = 0; i < TAM; i++) {
-  for (it = h.lista[i].begin(); it != h.lista[i].end(); it++) {
-   if (it -> get_codigo() % 4 == i) {
-    char nome_arquivo[FILENAME_MAX]; //usado para que a função consiga escrever todos os nomes dentro do char se nao ele nao comporta
-    sprintf(nome_arquivo, "Data-%d.txt", i); //função que realiza conversao de inteiros para char
-    arquivo.open(nome_arquivo, ios::app);
-
-
-   }
-
-   arquivo << "\n" << it -> get_codigo() << "\t" << it -> get_idade() << "\t" << it -> get_nome();
-   //cout << it->get_codigo();
-
-   arquivo.close();
-  }
- }
+void func_arquivo::cria_arquivo(){
+   ofstream arquivo;
+   list<pessoa>::iterator it;
+    for(int i=0;i<TAM;i++){
+        for(it=h.lista[i].begin();it!=h.lista[i].end();it++){
+            if(it->get_codigo() % TAM == i){
+                char nome_arquivo[FILENAME_MAX];//usado para que a função consiga escrever todos os nomes dentro do char se nao ele nao comporta
+                sprintf(nome_arquivo, "Data-%d.txt",i);//função que realiza conversao de inteiros para char
+                arquivo.open(nome_arquivo, ios::app);
+                arquivo << it->get_codigo() << "\t" << it->get_idade() << "\t" <<  it->get_nome() << "\n";
+            }
+        arquivo.close();
+        }
+    }
 }
 
 //função capaz de apagar os dados e limpar o arquivo
-void func_arquivo::limpar_arquivo() {
- ofstream arquivo;
- for (int i = 0; i < TAM; i++) {
-  char nome_arquivo[FILENAME_MAX]; //usado para que a função consiga escrever todos os nomes dentro do char se nao ele nao comporta
-  sprintf(nome_arquivo, "Data-%d.txt", i); //função que realiza conversao de inteiros para char
-  std::remove(nome_arquivo);
-  arquivo.close();
- }
+void func_arquivo::limpar_arquivo(){
+    ofstream arquivo;
+    for(int i=0;i<TAM;i++){
+        char nome_arquivo[FILENAME_MAX];//usado para que a função consiga escrever todos os nomes dentro do char se nao ele nao comporta
+        sprintf(nome_arquivo, "Data-%d.txt",i);//função que realiza conversao de inteiros para char
+        arquivo.open(nome_arquivo, ios::trunc);
+        arquivo.close();
+    }
 }
 
 //função capaz de iniciar o programa verificando a existenci de data
-void func_arquivo::inicia_data() {
- int codigo;
- int idade;
- string nome;
- ifstream arquivo;
+void func_arquivo::inicia_data(){
+    int codigo = 0;
+    int idade = 0;
+    string nome;
+    ifstream arquivo;
 
- for (int i = 0; i < TAM; i++) {
-  char nome_arquivo[FILENAME_MAX];
-  sprintf(nome_arquivo, "Data-%d.txt", i);
-  arquivo.open(nome_arquivo, ios:: in );
+    for(int i=0;i<TAM;i++){
 
-  if (!arquivo.fail()) {
-   while (!arquivo.eof()) {
-    arquivo >> codigo >> idade >> nome;
-    p.set_codigo(codigo);
-    p.set_idade(idade);
-    p.set_nome(nome);
-    h.insere_pessoa_hash(p);
-   }
-   cout << "Arquivo " << nome_arquivo << " importado com sucesso!!\n";
-   arquivo.close();
-  }
+        char nome_arquivo[FILENAME_MAX];
+        sprintf(nome_arquivo, "Data-%d.txt",i);
+        arquivo.open(nome_arquivo, ios::in);
 
- }
+            if(arquivo.fail() == 1){
+                cout<<"Arquivo inexistente!! erro fatal!!";
+            }
+            else{
+                while(!arquivo.eof()){
+
+                    arquivo >> codigo >> idade >> nome;
+
+                    p.set_codigo(codigo);
+                    p.set_idade(idade);
+                    p.set_nome(nome);
+                    h.insere_pessoa_hash(p);
+                    arquivo.close();
+                }
+
+            }
+
+    }
 
 }
--- TrabalhoHash/func_hash.h
+++ TrabalhoHash/func_hash.h
@@ -1,96 +1,99 @@
-#
-include < iostream > #include < list >
+#include <iostream>
+#include <list>
 
 
- #define TAM 4
+#define TAM 4
 
 using namespace std;
 
 pessoa pe;
 
 
-class hash {
+class hash{
 
- public:
-  list < pessoa > lista[TAM];
- hash();
- ~hash();
- int calcular_indice_hash(int codigo);
- void insere_pessoa_hash(pessoa p);
- void remove_pessoa_hash(int codigo);
- void consulta_hash(int codigo);
- void imprimi_hash();
- void salvar();
+    public:
+        list<pessoa>lista[TAM];
+        hash();
+        ~hash();
+        int calcular_indice_hash(int codigo);
+        void insere_pessoa_hash(pessoa p);
+        void remove_pessoa_hash(int codigo);
+        void consulta_hash(int codigo);
+        void imprimi_hash();
+        void salvar();
 };
 
 
 //reconstrutor
-hash::hash() {
+hash::hash(){
 
 }
 
 //destrutor
-hash::~hash() {
- cout << "\nTodas as pessoas cadastradas foram deletadas!!\n";
- for (int i = 0; i < TAM; i++) {
-  lista[i].clear();
- }
+hash::~hash(){
+    cout<< "\nTodas as pessoas cadastradas foram deletadas!!\n";
+    for(int i=0; i < TAM; i++){
+        lista[i].clear();
+    }
 }
 
 //realiza calculo para saber onde inserir on hash
-int hash::calcular_indice_hash(int codigo) {
- int indice = codigo % TAM;
- return indice;
+int hash::calcular_indice_hash(int codigo){
+    int indice = codigo % TAM;
+    return indice;
 }
 
 //insere pessoa no hash
-void hash::insere_pessoa_hash(pessoa p) {
- int indice = calcular_indice_hash(p.get_codigo());
- lista[indice].push_back(p);
+void hash::insere_pessoa_hash(pessoa p){
+    int indice = calcular_indice_hash(p.get_codigo());
+    lista[indice].push_back(p);
 }
 
 //remove pessoa do hash
-void hash::remove_pessoa_hash(int codigo) {
- list < pessoa > ::iterator it;
- int indice = calcular_indice_hash(codigo);
- for (it = lista[indice].begin(); it != lista[indice].end(); it++) {
-  //Procura o codigo do usuario num hashing especifico e o deleta.
-  if (it -> get_codigo() == codigo) {
-   cout << "Pessoa Deletada com sucesso!!.";
-   lista[indice].erase(it);
-   break;
-  } else {
-   cout << "\nEste código de pessoa nao existe!! Tente novamente!!\n";
-  }
- }
+void hash::remove_pessoa_hash(int codigo){
+    list<pessoa>::iterator it;
+    int indice = calcular_indice_hash(codigo);
+    for(it=lista[indice].begin();it!=lista[indice].end();it++){
+        //Procura o codigo do usuario num hashing especifico e o deleta.
+        if(it->get_codigo() ==codigo){
+            cout<< "Pessoa Deletada com sucesso!!.";
+            lista[indice].erase(it);
+            break;
+        }
+        else{
+            cout<< "\nEste código de pessoa nao existe!! Tente novamente!!\n";
+        }
+    }
 }
 
 //consulta o hash pelo codigo
-void hash::consulta_hash(int codigo) {
- list < pessoa > ::iterator it;
- int indice = calcular_indice_hash(codigo);
- for (it = lista[indice].begin(); it != lista[indice].end(); it++) {
-  //Procura o codigo do usuario num hashing especifico.
-  if (it -> get_codigo() == codigo) {
-   cout << "Resultado da consulta:";
-   it -> imprimir_pessoa();
-  } else {
-   cout << "Registro Inexistente.";
-  }
- }
+void hash::consulta_hash(int codigo){
+    list<pessoa>::iterator it;
+    int indice = calcular_indice_hash(codigo);
+    for(it=lista[indice].begin();it!=lista[indice].end();it++){
+        //Procura o codigo do usuario num hashing especifico.
+        if(it->get_codigo() ==codigo){
+            cout<< "Resultado da consulta:";
+            it->imprimir_pessoa();
+        }
+        else{
+            cout<< "Registro Inexistente.";
+        }
+    }
 }
 
 //exibi no termial oque temos no hash
-void hash::imprimi_hash() {
+void hash::imprimi_hash(){
 
- list < pessoa > ::iterator it;
- for (int i = 0; i < TAM; i++) {
-  for (it = lista[i].begin(); it != lista[i].end(); it++) {
-   it -> imprimir_pessoa();
-  }
- }
+    list<pessoa>::iterator it;
+    for(int i=0;i<TAM;i++){
+        for(it=lista[i].begin();it!=lista[i].end();it++){
+            it->imprimir_pessoa();
+        }
+    }
 }
 
-void hash::salvar() {
- //a.cria_arquivo();
+void hash::salvar(){
+    //a.cria_arquivo();
 }
+
